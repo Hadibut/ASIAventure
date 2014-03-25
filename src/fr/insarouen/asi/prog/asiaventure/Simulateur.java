d7c51c1ab9e20f5d5a6af9f5ce03d6a561dbafea
Drissi Reda <rdrissi@asi-testing>
2014-03-25 12:53:39 +0100

    TP7

---
+++ src/fr/insarouen/asi/prog/asiaventure/Simulateur.java
@@ -0,0 +1,144 @@
+package fr.insarouen.asi.prog.asiaventure;
+
+import fr.insarouen.asi.prog.asiaventure.elements.vivants.*;
+import fr.insarouen.asi.prog.asiaventure.elements.objets.*;
+import fr.insarouen.asi.prog.asiaventure.elements.objets.serrurerie.Serrure;
+import fr.insarouen.asi.prog.asiaventure.elements.structure.*;
+import fr.insarouen.asi.prog.asiaventure.elements.*;
+import fr.insarouen.asi.prog.asiaventure.*;
+import java.io.*;
+
+
+public class Simulateur {
+
+	private Monde monde;
+	private int dureeDuJeu;
+	private int tempsPourPrevenirLaFinDuJeu;
+
+
+	public Simulateur(Monde monde, int dureeDuJeu, int tempsPourPrevenirLaFinDuJeu) {
+
+		this.monde = monde;
+		this.dureeDuJeu = dureeDuJeu;
+		this.tempsPourPrevenirLaFinDuJeu = tempsPourPrevenirLaFinDuJeu;
+	}
+
+	public Simulateur(java.io.ObjectInputStream ois) 
+	throws IOException, java.lang.ClassNotFoundException {
+
+
+	}
+
+	public Simulateur(java.io.Reader reader) 
+	throws IOException, NomDEntiteDejaUtiliseDansLeMondeException {
+
+		StreamTokenizer st = new StreamTokenizer(reader);	
+
+		while (st.nextToken() != st.TT_EOF) {
+
+			switch (st.sval) {
+				case "Monde" : 
+					this.creerMonde(st);
+					break;
+				case "Piece" : 
+					this.creerPiece(st);
+					break;
+				case "PorteSerrure" : 
+					this.creerPorteSerrure(st);
+					break;
+				case "Porte" : 
+					this.creerPorte(st);
+					break;
+			}
+
+		}        
+	}
+
+	private void creerMonde(StreamTokenizer st)
+	throws IOException, NomDEntiteDejaUtiliseDansLeMondeException {
+
+		st.nextToken();
+		this.monde = new Monde(st.sval);
+	}
+
+	private void creerPiece(StreamTokenizer st)
+	throws IOException, NomDEntiteDejaUtiliseDansLeMondeException {
+
+		st.nextToken();
+		new Piece(st.sval, this.monde);
+	}
+
+	private void creerPorteSerrure(StreamTokenizer st)
+	throws IOException, NomDEntiteDejaUtiliseDansLeMondeException {
+
+		Serrure serrure = new Serrure(this.monde);		
+		
+		st.nextToken();
+		String nomPorte = st.sval;
+
+		st.nextToken();
+		String nomPieceA = st.sval;
+
+		st.nextToken();
+		String nomPieceB = st.sval;
+
+		new Porte(nomPorte, this.monde, serrure, (Piece) this.monde.getEntite(nomPieceA), (Piece) this.monde.getEntite(nomPieceB));
+	}
+
+	private void creerPorte(StreamTokenizer st)
+	throws IOException, NomDEntiteDejaUtiliseDansLeMondeException {
+		
+		st.nextToken();
+		String nomPorte = st.sval;
+
+		st.nextToken();
+		String nomPieceA = st.sval;
+
+		st.nextToken();
+		String nomPieceB = st.sval;
+
+		new Porte(nomPorte, this.monde, (Piece) this.monde.getEntite(nomPieceA), (Piece) this.monde.getEntite(nomPieceB));
+	}
+
+	// private void creerClef(StreamTokenizer st);
+
+	// private void creerJoueurHumain(StreamTokenizer st);
+
+
+	
+	public void enregistrer(java.io.ObjectOutputStream oos) throws java.io.IOException {
+
+
+	}	
+
+	// public void ajouterConditionsDeFin(java.util.Collection<ConditionDeFin> conditions) {
+
+
+	// }
+
+	// public void ajouterConditionDeFin(ConditionDeFin condition) {
+
+
+	// }
+
+	// public EtatDuJeu executerUnTour() throws java.lang.Throwable {
+
+
+	// }
+
+	// public EtatDuJeu executerNbTours(int n) throws java.lang.Throwable {
+
+
+	// }
+
+	// public EtatDuJeu executerJusquALaFin() throws java.lang.Throwable {
+
+
+	// }
+
+	// public void stopperJeu() {
+
+	// }
+
+
+}
---
+++ src/fr/insarouen/asi/prog/asiaventure/elements/vivants/JoueurHumain.java
@@ -0,0 +1,93 @@
+package fr.insarouen.asi.prog.asiaventure.elements.vivants;
+
+import fr.insarouen.asi.prog.asiaventure.elements.vivants.*;
+import fr.insarouen.asi.prog.asiaventure.elements.objets.*;
+import fr.insarouen.asi.prog.asiaventure.elements.structure.*;
+import fr.insarouen.asi.prog.asiaventure.elements.*;
+import fr.insarouen.asi.prog.asiaventure.*;
+import java.util.*;
+
+public class JoueurHumain extends Vivant {
+
+	private Piece piece;
+	private int pointVie;
+	private int pointForce;
+	private HashMap<String,Objet> objets;
+	private String ordre;
+
+
+/**
+	 * Crée un nouveau joueur humain
+	 * @param String nom
+	 * @param Monde monde
+	 * @param int pointVie
+	 * @param int pointForce
+	 * @param Piece piece
+	 * @param Objet... objets
+	 * @return JoueurHumain joueur humain créé
+	 * @throws NomDEntiteDejaUtiliseDansLeMondeException si une entité du même nom est déjà présente dans ce monde
+	 */
+	public JoueurHumain(String nom, Monde monde, int pointVie, int pointForce, Piece piece, Objet... objets)
+	throws NomDEntiteDejaUtiliseDansLeMondeException {
+
+		super(nom, monde, pointVie, pointForce, piece, objets);
+	}	
+
+	public void executer()  {
+
+		// String ordre1;
+		// String ordre2;
+		// String ordre3;
+
+		// switch () {
+		// 	case 1 : ordre1 = "ouvrir";
+		// 		commandeOuvrirPorte(ordre2);
+		// 		break;
+		// 	case 2 : ordre1 = "ouvrirAvec";	
+		// 		commandeOuvrirPorte(ordre2,ordre3)
+		// 		break;
+		// 	case 3 : ordre1 = "franchir";
+		// 		commandeFranchir(ordre2);
+		// 		break;	
+		// 	case 4 : ordre1 = "prendre";
+		// 		commandePrendre(ordre2);
+		// 		break;
+		// 	case 5 : ordre1 = "deposer";
+		// 		commandePoser(ordre2);		
+		// }
+	}	
+
+	public void setOrdre (String ordre) {
+
+		this.ordre = ordre ;
+
+	}
+
+	private void commandePrendre(String nomObjet) throws ObjetAbsentDeLaPieceException, ObjetNonDeplacableDeLaPieceException {
+
+		this.prendre(nomObjet);
+	}
+
+	private void commandePoser(String nomObjet) throws ObjetNonPossedeParLeVivantException {
+
+		this.deposer(nomObjet);
+	}
+	
+	private void commandeFranchir(String nomPorte) throws PorteFermeException, PorteInexistanteDansLaPieceException {
+
+		this.franchir(nomPorte);
+	}
+	
+	private void commandeOuvrirPorte(String nomPorte) throws ActivationImpossibleException {
+
+		this.piece.getPorte(nomPorte).activer();
+
+	} 	
+
+	private void commandeOuvrirPorte(String nomPorte, String nomObjet)throws ActivationImpossibleException{
+
+		this.piece.getporte(nomPorte).activerAvec(this.getObjet(nomObjet);
+
+	}  	
+
+}
